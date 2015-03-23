b3a4e01637da4b3d25e3b508410d9fb3e9b9de57
Dimon <Dreem_life@i.ua>
2015-03-23 11:59:16 +0200

    Версия от пятницы

--- JS1(calc_17.03).js
+++ JS1(calc_17.03).js
@@ -1,3 +1,147 @@
+var calculator = (function(){  
+        var carrentValue = null;
+
+        return {
+        add: function(x){
+            carrentValue += x;
+            },
+        susruct: function(x){
+            carrentValue -= x;
+            },
+        muitiply: function(x){
+            carrentValue *= x;
+            },
+        divide: function(x){
+            if(x===0){
+                alert('Делить на 0 нельзя');
+                carrentValue = 0;
+            }
+            else{
+            carrentValue /= x;
+                }
+            },
+        quadraticRoot: function(x){
+            if(x<0){
+                alert('Квадратный корень добываться только из положительных чисел')
+                carrentValue = 0;
+                }
+            else {
+                carrentValue = Math.sqrt(x);
+
+                }
+            },
+        getResult: function(){
+            return carrentValue
+            },
+        resetCalc: function(){
+            return carrentValue = null
+            }    
+        }
+
+} )()
+
+
+var handler = (function(){
+     var operation = null;
+    var text ='\n1.Сложить\n2.Вычислить\n3.Умножить\n4.Поделить\n5.Получить квадратный корень';
+
+         function chooseOperation(message){
+                var ourOperation = prompt(message);
+                var variantOfResult = ['1','2','3','4','5', null];
+                   for(var i=0; i<variantOfResult.length; i++){
+                        if(ourOperation === variantOfResult[i]){
+                           return operation = +ourOperation;    
+                        }       
+
+                    }
+                    return chooseOperation('****Нельза распознать операцию****\nПолайлуста корректно выберите операцию: '+text);
+
+            };
+
+         function peformOperation(number){
+                switch (operation){
+                case 1:
+                    calculator.add(number);
+                break;
+                case 2:
+                    calculator.susruct(number);
+                break;
+                case 3:
+                    calculator.muitiply(number);
+                break;
+                case 4:
+                    calculator.divide(number);
+                break;
+                case 5:
+                    calculator.quadraticRoot(number);
+                }
+            };
+
+          function  getNumber(message){
+                var number = prompt(message);
+                if(isNumeric(number)){
+                return +number;
+                }
+
+                return getNumber('Корректно введите число!!!');
+                
+                
+            };
+
+           function isNumeric(n){
+                return !isNaN(parseFloat(n)) && isFinite(n);
+            };
+
+
+            return {startHendler: function(){
+                while(true){
+                
+                        chooseOperation(' Если хотите воспользоваться калькулятором выберите операцию: ' + text)
+                        if (operation === 0){
+                            break;
+                        };   /// поставить параметр на выход при отмене
+
+                        if(calculator.getResult() === null){
+                        var a = getNumber('Введите первое число!');
+                            if(a===null){
+                            break;
+                         } else{
+                        calculator.add(a)
+                            }
+                        };
+
+
+                        if(operation === 5){
+                            peformOperation(calculator.getResult());
+                        } else{
+                        var b = getNumber('Введите второе число!')
+                            if(b===null){
+                            break;
+                         }
+                            peformOperation(b);
+                        }
+
+                        var result = confirm('Результат = ' + calculator.getResult() + '\nЖелаете продолжить?');
+                        if(result===false){
+                            break;
+                        };
+                        var memory = confirm('Хотите продолжить с полученным результатом?');
+                        if(memory===false){
+                            calculator.resetCalc();
+                        }                      
+                }
+            }
+        }
+
+
+})();
+
+handler.startHendler();
+
+
+
+
+/*
 var calculator = {
         carrentValue: null,
 
@@ -29,32 +173,34 @@ var calculator = {
 
                 }
             },
-
+        getResult: function(){
+            return calculator.carrentValue
+            },
+        resetCalc: function(){
+            return calculator.carrentValue = null
+            }    
 }
 
 
-var hendler = {
+var handler = {
     operation: null,
+    text: '\n1.Сложить\n2.Вычислить\n3.Умножить\n4.Поделить\n5.Получить квадратный корень',
 
-            chooseOperation: function(massege){
-                var ourOperation = prompt(massege);
-                    if (ourOperation !== null){
-                        ourOperation = +ourOperation;
-                        if(ourOperation === 1||ourOperation === 2||ourOperation === 3||ourOperation === 4||ourOperation === 5){
-                        return hendler.operation = ourOperation;
-                        }
+            chooseOperation: function(message){
+                var ourOperation = prompt(message);
+                var variantOfResult = ['1','2','3','4','5', null];
+                   for(var i=0; i<variantOfResult.length; i++){
+                        if(ourOperation === variantOfResult[i]){
+                           return handler.operation = +ourOperation;    
+                        }       
 
-                        return hendler.chooseOperation('****Нельза распознать операцию****\nПолайлуста корректно выберите операцию: \n1.Сложить\n2.Вычислить\n3.Умножить\n4.Поделить\n5.Получить квадратный корень');
-                        }
-                    else {
-                       return hendler.operation = 'End';
-                    }        
-                
-            
+                    }
+                    return handler.chooseOperation('****Нельза распознать операцию****\nПолайлуста корректно выберите операцию: '+handler.text);
+
             },
 
             peformOperation: function(number){
-                switch (hendler.operation){
+                switch (handler.operation){
                 case 1:
                     calculator.add(number);
                 break;
@@ -67,17 +213,18 @@ var hendler = {
                 case 4:
                     calculator.divide(number);
                 break;
-                
+                case 5:
+                    calculator.quadraticRoot(number);
                 }
             },
 
-            getNumber: function(massege){
-                var number = prompt(massege);
-                if(hendler.isNumeric(number)){
+            getNumber: function(message){
+                var number = prompt(message);
+                if(handler.isNumeric(number)){
                 return +number;
                 }
 
-                return hendler.getNumber('Корректно введите число!!!');
+                return handler.getNumber('Корректно введите число!!!');
                 
                 
             },
@@ -86,36 +233,41 @@ var hendler = {
                 return !isNaN(parseFloat(n)) && isFinite(n);
             },
 
+
             startHendler: function(){
                 while(true){
                 
-                        hendler.chooseOperation(' Если хотите воспользоваться калькулятором выберите операцию: \n1.Сложить\n2.Вычислить\n3.Умножить\n4.Поделить\n5.Получить квадратный корень');
-                        if (hendler.operation === 'End'){
+                        handler.chooseOperation(' Если хотите воспользоваться калькулятором выберите операцию: '+handler.text);
+                        if (handler.operation === 0){
                             break;
                         };   /// поставить параметр на выход при отмене
 
-                        if(calculator.carrentValue === null){
-                        var a = hendler.getNumber('Введите первое число!');
+                        if(calculator.getResult() === null){
+                        var a = handler.getNumber('Введите первое число!');
                         calculator.add(a)
                         };
-                        if(hendler.operation === 5){
-                            calculator.quadraticRoot(calculator.carrentValue);
+
+
+                        if(handler.operation === 5){
+                            handler.peformOperation(calculator.getResult());
                         } else{
-                        var b = hendler.getNumber('Введите второе число!')
-                            hendler.peformOperation(b);
+                        var b = handler.getNumber('Введите второе число!')
+                            handler.peformOperation(b);
                         }
 
-                        var result = confirm('Результат = ' + calculator.carrentValue + '\nЖелаете продолжить?');
+
+
+                        var result = confirm('Результат = ' + calculator.getResult() + '\nЖелаете продолжить?');
                         if(result===false){
                             break;
                         };
                         var memory = confirm('Хотите продолжить с полученным результатом?');
                         if(memory===false){
-                            calculator.carrentValue = null;
+                            calculator.resetCalc();
                         }                      
                 }
             }
 }
-hendler.startHendler();
+handler.startHendler();  */
 
-///df sdf asdf 
+
